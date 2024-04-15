695787851bb85fed757de7d36768f51ea3683492
GeekyNaved <navedahmed040@gmail.com>
2024-04-15 12:09:34 +0530

    adding pagination

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [newsData, setNewsData] = useState([]);
   const [query, setQuery] = useState("");
   const [nbPages, setNbPages] = useState(0);
+  const [page, setPage] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const API_URL = "http://hn.algolia.com/api/v1/search?";
 
@@ -23,10 +24,12 @@ function App() {
         // console.log("response", response?.data?.hits);
         if (response?.data?.hits?.length > 0) {
           setNewsData(response?.data?.hits);
-        } else
+          setNbPages(response?.data?.nbPages);
+        } else {
           toast.error("No results found.", {
             id: "noResults",
           });
+        }
       })
       .catch((error) => {
         setIsLoading(false);
@@ -42,12 +45,40 @@ function App() {
   };
 
   useEffect(() => {
-    getNews(`${API_URL}query=${query}&page=${nbPages}`);
-  }, [nbPages, query]);
+    getNews(`${API_URL}query=${query}&page=${page}`);
+  }, [page, query]);
 
   return (
-    <div className="min-h-screen px-5 md:p-0 bg-slate-100 flex flex-col items-center">
+    <div className="min-h-screen px-5 py-5 bg-slate-100 flex flex-col items-center">
       <Search searchValue={query} onSearch={handleSearch} />
+      {/* pagination */}
+      <div className="flex gap-2 items-center py-5">
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page == 0 ? true : false}
+          className={
+            page == 0
+              ? "opacity-20 border-2 border-slate-900 px-4 py-2 rounded-md"
+              : "border-2 border-slate-900 px-4 py-2 rounded-md hover:bg-slate-200 transition duration-300"
+          }
+        >
+          Prev
+        </button>
+        <p>
+          {page + 1} of {nbPages}
+        </p>
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={page == nbPages - 1 ? true : false}
+          className={
+            page == nbPages - 1
+              ? "opacity-20 border-2 border-slate-900 px-4 py-2 rounded-md"
+              : "border-2 border-slate-900 px-4 py-2 rounded-md hover:bg-slate-200 transition duration-300"
+          }
+        >
+          Next
+        </button>
+      </div>
       {isLoading ? (
         <div>
           <Loader
