315cc964db0e49dfd77d84ddc56ee9c5f8246995
GeekyNaved <navedahmed040@gmail.com>
2024-04-15 09:50:03 +0530

    adding save button toggle

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,14 @@
-import {BsBookmarkStarFill} from "react-icons/bs";
+import {useState} from "react";
+import {BsBookmarkCheckFill} from "react-icons/bs";
+import {BsBookmark} from "react-icons/bs";
 
 const Card = ({title, comments, author, url}) => {
+  const [saved, setSaved] = useState(false);
+  const toggleSave = () => {
+    setSaved(!saved);
+  };
   return (
-    <div className="text-left bg-gray-50 shadow-lg px-3 md:px-5 py-2 rounded flex-1">
+    <div className="text-left bg-gray-50 shadow-lg px-3 md:px-5 py-5 rounded flex-1">
       <div className="py-2">
         <p className="text-gray-900 md:text-3xl text-2xl font-medium">
           {title}
@@ -13,12 +19,20 @@ const Card = ({title, comments, author, url}) => {
         <span className="text-xs font-medium">{comments} comments</span>
       </p>
       {url && (
-        <div className="flex justify-between items-center">
-          <button className="flex items-center gap-1">
-            <BsBookmarkStarFill />
-            <p className="font-bold">save</p>
+        <div className="flex justify-between items-center pt-4">
+          <button className="flex items-center gap-1" onClick={toggleSave}>
+            {saved ? (
+              <div className="text-2xl">
+                <BsBookmarkCheckFill />
+              </div>
+            ) : (
+              <div className="text-2xl">
+                <BsBookmark />
+              </div>
+            )}
+            <p className="font-bold md:text-2xl">save</p>
           </button>
-          <div className="py-3">
+          <div>
             <a
               href={url}
               target="_blank"
